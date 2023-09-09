e1707ee77852bf443ce80c8be31714b9185e1c35
vikharevam <ViharevAMIP@gmail.com>
2023-09-09 13:35:35 +0300

    Add new method

--- Magic Ball/ViewController.swift
+++ Magic Ball/ViewController.swift
@@ -16,6 +16,9 @@ class ViewController: UIViewController {
         viewAnsver.image = UIImage(named: "Ball_Inactive")
     }
     
+    
+    
+    
     @IBOutlet weak var viewAnsver: UIImageView!
     
     
@@ -26,18 +29,42 @@ class ViewController: UIViewController {
         UIImage(named:"ball_Yes")!,
         UIImage(named:"ball5")!]
     
-    @IBAction func pressButton(_ sender: UIButton) {
-        
-        viewAnsver.image = ballAnswerArray.randomElement()
-        
-        UIImpactFeedbackGenerator(style: .medium).impactOccurred()
-    }
+    //    @IBAction func pressButton(_ sender: UIButton) {
+    //
+    //        viewAnsver.image = ballAnswerArray.randomElement()
+    //
+    //        UIImpactFeedbackGenerator(style: .medium).impactOccurred()
+    //    }
     
     override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-        viewAnsver.image = UIImage(named: "Ball_Inactive")
-
-//        AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+        //        viewAnsver.image = UIImage(named: "Ball_Inactive")
+        answerMode()
+        UIImpactFeedbackGenerator(style: .heavy).impactOccurred()
+        
+    }
+    
+    func answerMode (){
+        shakeImage()
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1.0) {
+            self.viewAnsver.image = self.ballAnswerArray.randomElement()
+           
+        }
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 4.0) {
+            self.viewAnsver.image = UIImage(named: "Ball_Inactive")
+        }
+ 
+    }
+    
+    func shakeImage() {
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.05
+        animation.repeatCount = 5
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: viewAnsver.center.x - 10, y: viewAnsver.center.y))
+        animation.toValue = NSValue(cgPoint: CGPoint(x: viewAnsver.center.x + 10, y: viewAnsver.center.y))
+        viewAnsver.layer.add(animation, forKey: "position")
     }
     
 }
+
 
